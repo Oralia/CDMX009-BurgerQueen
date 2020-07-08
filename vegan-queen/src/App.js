d428bda8429c885c386477c4c596dc3eea92ad8c
JimenaMV <jimenamanzanares.v@gmail.com>
2020-07-08 01:54:17 -0500

    Implementing Router

--- vegan-queen/src/App.js
+++ vegan-queen/src/App.js
@@ -1,14 +1,64 @@
 import React from 'react';
-import background from './img/background.png'
+//Router
+import {
+  BrowserRouter as Router,
+  //Con switch se crea el contenido dinámico, adentro va el route 
+  Switch,
+  Route,
+  //Link es un ancla
+  Link,
+  NavLink
+} from "react-router-dom";
+
+
 import './App.css';
+import Main from './components/Main';
+import Waiter from './components/Waiter';
 
 function App() {
   return (
-    <div className="App">
-        <img src={background}/>
-        <h1>Pantalla principal</h1>
-        
+    <Router>
+      <div className="container mt-5">
+        <div className='btn-group'>
+          <Link to = '/' className= 'btn btn-dark'>
+            Main
+          </Link>
+          <NavLink to = '/rol' className= 'btn btn-dark' activeClassName='active'>
+            M & C
+          </NavLink>
+        </div>
+        <h1>Elementos permanentes (navbar o quizá los iconos)</h1>
+        <hr />
+          <Switch>
+            <Route path = "/rol">
+              Escoge entre mesero o cocinero
+            </Route>
+            <Route path = "/waiter">
+              <Waiter />
+            </Route>
+            <Route path = "/menu">
+              Pantalla menu: desayuno y burger
+            </Route>
+            <Route path = "/menu-breakfast">
+              Pantalla breakfast
+            </Route>
+            <Route path = "/menu-burger">
+              Pantalla burger
+            </Route>
+            <Route path = "/chef">
+              pantalla chef
+            </Route>
+            <Route path = "/orders">
+              Pantalla comandas, aqui van los componentes
+            </Route>
+            <Route path = "/">
+              {/* Aqui se llaman los componentes */}
+              <Main />
+            </Route>
+          </Switch>
     </div>
+    </Router>
+    
   );
 }
 
---
+++ vegan-queen/src/components/Waiter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Waiter = () => {
+    return (
+        <div>
+            <h1>waiter</h1>
+        </div>
+    )
+}
+
+export default Waiter
