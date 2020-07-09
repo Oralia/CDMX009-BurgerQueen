1a84a154c231f210c7fb37a5e48f55e9a071e857
JimenaMV <jimenamanzanares.v@gmail.com>
2020-07-09 12:47:11 -0500

    Adding new images

--- vegan-queen/src/components/Main.jsx
+++ vegan-queen/src/components/Main.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import logo from '../img/logo_vegan.png'
 import chef from '../img/chef-animate (1).svg'
+import { Link } from "react-router-dom"
+//import '../App.css';
 
 const Main = () => {
     return (
-        <div>
-            <img src={chef} alt=""/>
-            <img src={logo} alt=""/>
-        </div>
+        <Fragment>
+            <div>
+                <img src={ chef } alt="logotipo vegan burger"/>
+            </div>
+            <Link to = '/role'>
+                <img id='logo' src={ logo } alt=""/>
+            </Link>
+        </Fragment>
     )
 }
 
