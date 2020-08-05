95b15bc04fdb7b5a039965b836643a04044d1529
Oralia <oraliagarrido@gmail.com>
2020-08-04 23:24:15 -0500

    add and delete products

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -10,105 +10,102 @@ import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons' */
 import InfoClients from "../utils/InfoClients";
 import InfoTotal from "../utils/InfoTotal";
 
-const MenuBurger = ({ 
+const MenuBurger = ({
   dataHamburger,
   dataIngredients,
   dataExtras,
   dataDrinks,
   dataAccompaniments,
-  order, 
-  addingProduct, 
-  deletingProduct 
+  order,
+  addingProductToOrder,
+  deletingProductToOrder,
 }) => {
   return (
     <Fragment>
       <Navbar />
       <div className={styles.logoUp}>
-        <Boton 
-          image={mburger} 
-          adress="/menu-burger"/>
+        <Boton image={mburger} adress="/menu-burger" />
       </div>
       <InfoClients />
-        <section className={styles.container}>
-          <strong>HAMBURGUESAS</strong>
-          <br/>
-          <div class={styles.menuBurger}>
-            <div className={styles.containerHamburguer}>
-              <p>1.Tipo</p>
-              {dataHamburger.map((product) => (
-                <Item
-                  key={product.id}
-                  product={product}
-                  /* order={order} */
-                  addingProduct={addingProduct}
-                  deletingProduct={deletingProduct}
-                />
-              ))}
-            </div>
+      <section className={styles.container}>
+        <strong>HAMBURGUESAS</strong>
+        <br />
+        <div class={styles.menuBurger}>
+          <div className={styles.containerHamburguer}>
+            <p>1.Tipo</p>
+            {dataHamburger.map((product) => (
+              <Item
+                key={product.id}
+                product={product}
+                /* order={order} */
+                addingProductToOrder={addingProductToOrder}
+                deletingProductToOrder={deletingProductToOrder}
+              />
+            ))}
+          </div>
 
-            <div className={styles.genericCont}>
-               <p>2.Ingrediente</p>
-               <div className={styles.containerIngredients}>
-                {dataIngredients.map((product) => (
+          <div className={styles.genericCont}>
+            <p>2.Ingrediente</p>
+            <div className={styles.containerIngredients}>
+              {dataIngredients.map((product) => (
                 <Item
                   key={product.id}
                   product={product}
                   /* order={order} */
-                  addingProduct={addingProduct}
-                  deletingProduct={deletingProduct}
-                />
-                ))}
-              </div>
-            </div>
-
-            <div className={styles.containerExtras}>
-            <p>3.Extra</p>
-              {dataExtras.map((product) => (
-                <Item
-                  key={product.id}
-                  product={product}
-                  /* order={order} */
-                  addingProduct={addingProduct}
-                  deletingProduct={deletingProduct}
+                  addingProductToOrder={addingProductToOrder}
+                  deletingProductToOrder={deletingProductToOrder}
                 />
               ))}
             </div>
           </div>
 
-          <div class={styles.bebidasAccom}>
+          <div className={styles.containerExtras}>
+            <p>3.Extra</p>
+            {dataExtras.map((product) => (
+              <Item
+                key={product.id}
+                product={product}
+                /* order={order} */
+                addingProductToOrder={addingProductToOrder}
+                deletingProductToOrder={deletingProductToOrder}
+              />
+            ))}
+          </div>
+        </div>
 
-            <div className={styles.genericCont}>
-              <p>BEBIDAS</p>
-              <div className={styles.containerDrinks}>
+        <div class={styles.bebidasAccom}>
+          <div className={styles.genericCont}>
+            <p>BEBIDAS</p>
+            <div className={styles.containerDrinks}>
               {dataDrinks.map((product) => (
                 <Item
                   key={product.id}
                   product={product}
                   /* order={order} */
-                  addingProduct={addingProduct}
-                  deletingProduct={deletingProduct}
+                  aaddingProductToOrder={addingProductToOrder}
+                  deletingProductToOrder={deletingProductToOrder}
                 />
               ))}
-              </div>
             </div>
+          </div>
 
-            <div className={styles.genericCont}>
-              <p>ACOMPAÑAMIENTOS</p>
-              <div className={styles.containerAccompaniments}>
-                {dataAccompaniments.map((product) => (
-                  <Item
+          <div className={styles.genericCont}>
+            <p>ACOMPAÑAMIENTOS</p>
+            <div className={styles.containerAccompaniments}>
+              {dataAccompaniments.map((product) => (
+                <Item
                   key={product.id}
                   product={product}
                   /* order={order} */
-                  addingProduct={addingProduct}
-                  deletingProduct={deletingProduct}
+                  addingProductToOrder={addingProductToOrder}
+                  deletingProductToOrder={deletingProductToOrder}
                 />
-                ))}
-              </div> 
+              ))}
             </div>
           </div>
-        </section>
-        <InfoTotal />
+        </div>
+      </section>
+      <InfoTotal />
     </Fragment>
   );
 };
--- src/components/utils/Item.js
+++ src/components/utils/Item.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from '../utils/Buton/style.module.css'
-import wrongMark from '../assets/img/wrong-mark.svg'
+import styles from "../utils/Buton/style.module.css";
+import wrongMark from "../assets/img/wrong-mark.svg";
 
 const images = {
   "sandwich-hummus": require("../assets/img/03-sandwich-hummus.svg"),
@@ -9,47 +9,52 @@ const images = {
   "natural-juice": require("../assets/img/03-jugo-natural.svg"),
   "simple-burger": require("../assets/img/04a-hamburguesa-sencilla.svg"),
   "double-burger": require("../assets/img/04a-hamburguesa-doble.svg"),
-  "portobello": require("../assets/img/04b-portobello.svg"),
-  "soya": require("../assets/img/04b-soya.svg"),
-  "grains": require("../assets/img/04b-granos.svg"),
-  "guacamole": require("../assets/img/04c-guacamole.svg"),
-  "veganessa": require("../assets/img/04c-veganesa.svg"),
-  "fries": require("../assets/img/06-papas.svg"),
-  "salad": require("../assets/img/06-ensalada.svg"),
-  "hummus": require("../assets/img/06-hummus.svg"),
-  "tapioca": require("../assets/img/05-tapioca.svg"),
-  "water": require("../assets/img/05-agua-del-dia.svg"),
+  portobello: require("../assets/img/04b-portobello.svg"),
+  soya: require("../assets/img/04b-soya.svg"),
+  grains: require("../assets/img/04b-granos.svg"),
+  guacamole: require("../assets/img/04c-guacamole.svg"),
+  veganessa: require("../assets/img/04c-veganesa.svg"),
+  fries: require("../assets/img/06-papas.svg"),
+  salad: require("../assets/img/06-ensalada.svg"),
+  hummus: require("../assets/img/06-hummus.svg"),
+  tapioca: require("../assets/img/05-tapioca.svg"),
+  water: require("../assets/img/05-agua-del-dia.svg"),
   "milk-coconut": require("../assets/img/05-leche-coco.svg"),
-  "te-chai-burger": require("../assets/img/05-te-chai.svg")
-}
+  "te-chai-burger": require("../assets/img/05-te-chai.svg"),
+};
 
 const Item = ({
   product,
+  order,
   addingProductToOrder,
-  deletingProduct,
+  deletingProductToOrder,
+  Data,
 }) => {
   return (
     <div className={styles.containerItem}>
-      <img 
-        src={images[product.image]} 
-        className={styles.btnNav} alt="" 
+      <img
+        src={images[product.image]}
+        className={styles.btnNav}
+        alt=""
         onClick={() => addingProductToOrder(product)}
       />
-      
-      <img 
-      src={wrongMark} 
-      className={styles.iconProduct}
-      onClick={() => deletingProduct(product.id)}
-      alt=""
+      <p>{product.productName}</p>
+      <h6>${product.cost}</h6>
+      <p>{product.quantity}</p>
+      <img
+        src={wrongMark}
+        className={styles.iconProduct}
+        onClick={() => deletingProductToOrder(product)}
+        alt=""
       />
-      
+
       {/* <FontAwesomeIcon 
         icon = { faTimesCircle }
         className={styles.iconProduct}
-        onClick={() => deletingProduct(product.id)}
+        onClick={() => deletingProductToOrder(product)}
         
       /> */}
-      {/* <button type="button" onClick={() => deletingProduct(product.id)}>
+      {/* <button type="button" onClick={() => deletingProductToOrder(product)}>
         <FontAwesomeIcon 
         icon = { faTimesCircle }
         color='#F5C6C6'
@@ -61,3 +66,4 @@ const Item = ({
 
 export default Item;
 
+//<h6>{product.kind}</h6>
